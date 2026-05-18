746ce6a454120d50bdd949d4e11305ed0389ed27
Ryan Stokes <ryanstokes560@gmail.com>
2026-05-18 21:50:34 +0100

    Create class functionality added and renders info on teacher dash

--- src/public/js/teacherScripts.js
+++ src/public/js/teacherScripts.js
@@ -1,9 +1,40 @@
 /**
  * Author: Ryan Stokes
  * File: teacherScripts.js
- * Last Modified: 2026-05-17
+ * Last Modified: 2026-05-18
  */
 
 document.addEventListener("DOMContentLoaded", () => {
-  
+    initAddClass();
 });
+
+function initAddClass() {
+    const form = document.getElementById("addClassForm");
+    if (!form) return;
+    form.addEventListener("submit", handleAddClass);
+}
+
+async function handleAddClass(e) {
+
+    e.preventDefault();
+
+    const form = e.target;
+
+      const res = await fetch("/api/teacher/create-class", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+            name: form.name.value,
+      })
+  });
+
+  // If class creation is successful (response status 200), redirect to the teacher dashboard, otherwise show an error message
+  if (res.ok) {
+      window.location.href = "/teacher/dashboard";
+  } else {
+      const data = await res.json();
+      alert(data.error || "Failed to create class");
+  }
+};
