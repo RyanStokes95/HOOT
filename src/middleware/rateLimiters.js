b5a8b8c07c7ee719a185c4ba785abec37727c2be
Ryan Stokes <ryanstokes560@gmail.com>
2026-05-19 19:48:44 +0100

    Added fix to rate limiting to disable in test/int test enviornment

--- src/middleware/rateLimiters.js
+++ src/middleware/rateLimiters.js
@@ -1,17 +1,44 @@
 /**
+
  * Author: Ryan Stokes
+
  * File: rateLimiters.js
+
  * Last Modified: 2026-05-20
+
  */
 
 import rateLimit from "express-rate-limit";
 
+// Define a flag to check if the environment is test or integration, used to skip rate limiting during tests
+const isTest =
+    process.env.NODE_ENV === "test" ||
+    process.env.NODE_ENV === "integration";
+
+
 export const genericLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per window
+
+    // 15 minutes
+    windowMs: 15 * 60 * 1000, 
+
+    // limit each IP to 100 requests per window
+    max: 100, 
+
+    // Skip rate limiting in test environments to avoid interference with automated tests
+    skip: () => isTest
+
 });
 
+
 export const authLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 5, // limit each IP to 5 requests per window
+
+    // 15 minutes
+    windowMs: 15 * 60 * 1000, 
+
+    // limit each IP to 10 requests per window
+    max: 10, 
+
+    // Skip rate limiting in test environments to avoid interference with automated tests
+    skip: () => isTest
+
 });
